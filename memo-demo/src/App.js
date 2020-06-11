de7108b2a92adb1e7dc578c102931a9bef61d877
rushi444 <rushi44@gmail.com>
2020-06-10 19:07:55 -0700

    useMemo hook tutorial

--- memo-demo/src/App.js
+++ memo-demo/src/App.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 
-import { NameInput, NameInputMemo } from './NameInput';
-import { AgeInput, AgeInputMemo } from './AgeInput';
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
 
-function App() {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
+  // const doubleNumber = slowFunction(number);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black',
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>Memo Demo</header>
-      <NameInputMemo name={name} setName={setName} />
-      <AgeInputMemo age={age} setAge={setAge} />
-      <p>
-        Hi, my name is {name}, I am {age} years old
-      </p>
+    <div style={{ textAlign: 'center' }}>
+      <input
+        type='number'
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change color
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </div>
   );
 }
 
-export default App;
+function slowFunction(num) {
+  console.log('slowfunction');
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+}
