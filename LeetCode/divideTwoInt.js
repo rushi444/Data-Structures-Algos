f1edec42dc2897ed0ff18e0ef899318d79f5b800
rushi444 <rushi44@gmail.com>
2020-06-21 20:18:08 -0700

    divide two ints

---
+++ LeetCode/divideTwoInt.js
@@ -0,0 +1,20 @@
+var divide = function (dividend, divisor) {
+  if (dividend === -Math.pow(2, 31) && divisor === -1) {
+    return Math.pow(2, 31) - 1;
+  }
+  if (divisor === 1) {
+    return dividend;
+  }
+  let isNegative = false;
+  if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+    isNegative = true;
+  }
+  let newDividend = Math.abs(dividend);
+  const newDivisor = Math.abs(divisor);
+  let result = 0;
+  while (newDividend >= newDivisor) {
+    newDividend = newDividend - newDivisor;
+    result++;
+  }
+  return isNegative ? -result : result;
+};
