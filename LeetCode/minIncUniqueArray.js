249851de8166af35a79755c8dede663a550026c2
Rushi Arumalla <49988869+rushi444@users.noreply.github.com>
2020-06-30 18:49:44 -0700

    Create minIncUniqueArray.js

---
+++ LeetCode/minIncUniqueArray.js
@@ -0,0 +1,21 @@
+const minIncrementForUnique = (A) => {
+
+    A.sort((a,b) => (a - b))
+
+    let result = 0
+
+    for(let i = 1; i < A.length; i++) {
+
+        while(A[i] <= A[i - 1]) {
+
+            A[i] += 1
+
+            result += 1
+
+        }
+
+    }
+
+    return result
+
+};
